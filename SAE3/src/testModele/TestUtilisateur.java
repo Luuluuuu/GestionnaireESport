7b37992ae4e534848d8c295884d012c7c206ebd6
guillaume <bertrandguillaume524@gmail.com>
2022-12-07 09:50:33 +0100

    ajout des tests

--- SAE3/src/testModele/TestUtilisateur.java
+++ SAE3/src/testModele/TestUtilisateur.java
@@ -2,30 +2,57 @@ package testModele;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import modele.Connexion;
+import modele.Poule;
 import modele.Utilisateur;
+import modele.Utilisateur.Profil;
 
 public class TestUtilisateur {
-	private Utilisateur u ;
+	private Utilisateur utilisateur ;
 	
 	@Before
 	public void setUp() {
-		u = new Utilisateur();
+		this.utilisateur = new Utilisateur();
 	}
 	
 	@After
 	public void tearDown() {
-		u = null;
+		this.utilisateur = null;
 	}
-	/*
+	
 	@Test
-	public void testMotDePasseCorrect() throws SQLException {
-		assertTrue(u.mdpCorrect("admin", "$esporter")); 
-	}*/
+	public void testConstructeur() {
+		assertTrue(this.utilisateur instanceof Utilisateur);
+	}
+
+	@Test
+	public void testAjouterUtilisateur() {
+		this.utilisateur.ajouterUtilisateur(("vidal"+"."+"jean"), "$iutinfo", Profil.ARBITRE, 1);
+		Connexion c = Connexion.getInstance();
+		//faire la requete par rapport a un utilisateur et tester tout les choix
+		ResultSet rs = c.retournerRequete("SELECT * FROM sae_user,sae_arbitre WHERE sae_user.idarbitre=sae_arbitre.idarbitre and sae_arbitre.nomarbitre='duval'");
+		try {
+			while (rs.next()) {
+				assertTrue(rs.getString(1).equals("vidal"));
+				
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void mdpCorrect() {
+		
+	}
 
 }
