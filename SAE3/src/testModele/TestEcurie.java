ec6cbf8a947b01c1f3da038cae334c8a6141639d
guillaume <bertrandguillaume524@gmail.com>
2022-12-07 23:30:41 +0100

    ajout des tests

---
+++ SAE3/src/testModele/TestEcurie.java
@@ -0,0 +1,123 @@
+package testModele;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import modele.Connexion;
+import modele.Ecurie;
+import modele.Equipe;
+import modele.Jeu;
+import modele.Joueur;
+
+public class TestEcurie {
+
+	private Ecurie ecurie;
+	private List<Joueur> joueurs;
+	private List<Equipe> equipes;
+	private Joueur joueur;
+	private Jeu jeu;
+	private Equipe equipe;
+	
+	@Before
+	public void setUp() throws Exception{
+		this.joueurs = new ArrayList<Joueur> ();
+		this.equipes = new ArrayList<Equipe> ();
+		this.ecurie = new Ecurie(1,"Kcorp");
+		this.joueur = new Joueur("maltra","léo","léo123","06/02/98","Allemagne");
+		this.jeu = new Jeu(1,"league of legends", 5);
+		this.equipe = new Equipe("DRX",12, jeu, joueurs);
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		this.joueurs = null;
+		this.equipes = null;
+		this.ecurie = null;
+		this.joueur = null;
+		this.equipe = null;
+	}
+	
+	@Test
+	public void testConstructeur() {
+		assertTrue(this.ecurie instanceof Ecurie);
+	}
+	
+	@Test
+	public void testCreeLogin() {
+		Ecurie ec = new Ecurie(1, "T1");
+		ec.creerLogin("$iutinfo");
+		Connexion c = Connexion.getInstance();
+		ResultSet rs = c.retournerRequete("SELECT * FROM sae_user,sae_ecurie WHERE sae_user.idecurie=sae_ecurie.idecurie and sae_ecurie.nomecurie='T1'");
+		try {
+			while (rs.next()) {
+				assertTrue(rs.getString(2).equals("T1"));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetId() {
+		assertEquals(this.ecurie.getID(), 1);
+	}
+	
+	@Test
+	public void testGetNom() {
+		assertEquals(this.ecurie.getNom(), "Kcorp");
+	}
+	
+	@Test
+	public void testSetAnneeDeCreation() {
+		this.ecurie.setAnneeDeCreation(2022);
+		assertEquals(this.ecurie.getAnneeDeCreation(), 2022);
+	}
+	
+	@Test
+	public void testSupprimerEquipe() {
+		this.ecurie.ajouterEquipe(this.equipe);
+		this.ecurie.supprimerEquipe(this.equipe);
+		assertTrue(this.equipes.isEmpty());
+	}
+	
+	@Test
+	public void testAjouterEquipe() {
+		this.ecurie.ajouterEquipe(this.equipe);
+	}
+	
+	@Test
+	public void testGetEquipe() {
+		this.joueurs.add(this.joueur);
+		this.ecurie.ajouterEquipe(this.equipe);
+		assertTrue(this.ecurie.getEquipes().contains(this.equipe));
+	}
+	
+	@Test
+	public void testGetListeEquipe() {
+		assertEquals(this.ecurie.getEquipes(), this.equipes);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSupressionEquipeAbscente() {
+		this.ecurie.supprimerEquipe(this.equipe);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAjoutEquipePresente() {
+		this.ecurie.ajouterEquipe(this.equipe);
+		this.ecurie.ajouterEquipe(this.equipe);
+	}
+	
+
+
+}
---
+++ SAE3/src/testModele/TestEquipe.java
@@ -0,0 +1,106 @@
+package testModele;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import modele.Equipe;
+import modele.Jeu;
+import modele.Joueur;
+import modele.Poule;
+
+public class TestEquipe {
+
+	private Equipe equipe;
+	private List<Joueur> joueurs= new ArrayList<Joueur> ();
+    private Jeu jeu;
+    private List<Poule> poules;
+    private List<Equipe> equipes;
+    private Poule poule;
+    private Joueur joueur;
+    
+	@Before
+	public void setUp() throws Exception {
+		this.equipe = new Equipe("fnatic", 32, jeu, joueurs);
+		this.poules = new ArrayList<Poule> ();
+		this.equipes = new ArrayList<Equipe> ();
+		this.poule = new Poule(this.equipes, true);
+		this.joueur = new Joueur("maltra","léo","léo123","06/02/98","Allemagne");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.equipe = null;
+		this.joueurs = null;
+		this.jeu = null;
+		this.poule = null;
+	}
+
+	@Test
+	public void testConstructeur() {
+		assertTrue(this.equipe instanceof Equipe);
+	}
+	
+	@Test
+	public void testGetNom() {
+		assertEquals(this.equipe.getNom(), "fnatic");
+	}
+	
+	@Test
+	public void testSetNom() {
+		this.equipe.setNom("t1");
+		assertEquals(this.equipe.getNom(), "t1");
+	}
+	
+	@Test
+	public void testGetjoueur() {
+		assertEquals(this.equipe.getJoueurs(),this.joueurs);
+	}
+	
+	@Test
+	public void testAjouterJoueur() {
+		this.equipe.ajouterJoueur(joueur);
+		assertTrue(this.joueurs.contains(this.joueur));
+	}
+	
+	@Test
+	public void testGetJeu() {
+		assertEquals(this.equipe.getJeu(), this.jeu);
+	}
+	
+	@Test
+	public void testSetJeu() {
+		Jeu j = new Jeu(2,"Rc",12);
+		this.equipe.setJeu(j);
+		assertEquals(this.equipe.getJeu(),j);
+	}
+	
+	@Test
+	public void testGetPointsChampionnat() {
+		assertEquals(this.equipe.getPointsChampionnat(), 32);
+	}
+	
+	@Test
+	public void testGetPoule() {
+		assertEquals(this.equipe.getPoule(), this.poules);
+	}
+	
+	@Test
+	public void testAjouterPoule() {
+		this.equipe.ajouterPoule(this.poule);
+		assertTrue(this.equipe.getPoule().contains(this.poule));
+		
+	}
+	
+	@Test
+	public void testAjouterPointsChampionnat() {
+		this.equipe.ajouterPointsChampionnat(5);
+		assertEquals(this.equipe.getPointsChampionnat(), 37);
+	}
+
+}
