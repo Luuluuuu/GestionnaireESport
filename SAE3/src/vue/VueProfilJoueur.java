d555423140e467ea281ed90614e7de17c762cdd9
Luuluuuu <lucille4713@gmail.com>
2023-01-06 15:15:34 +0100

    ajout 3 pages (inscription tournoi/profil joueur/tournois joueurs)

---
+++ SAE3/src/vue/VueProfilJoueur.java
@@ -0,0 +1,263 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class VueProfilJoueur {
+
+	private JFrame fenetreProfilJoueur;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VueProfilJoueur window = new VueProfilJoueur();
+					window.fenetreProfilJoueur.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private VueProfilJoueur() {
+		fenetreProfilJoueur = new JFrame();
+		fenetreProfilJoueur.getContentPane().setBackground(Couleur.BLEU1);
+		fenetreProfilJoueur.setResizable(false);
+		fenetreProfilJoueur.setBounds(100, 100, 1400, 900);
+		fenetreProfilJoueur.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelDeconnexion = new JPanel();
+		panelDeconnexion.setBackground(Color.WHITE);
+		fenetreProfilJoueur.getContentPane().add(panelDeconnexion, BorderLayout.NORTH);
+		panelDeconnexion.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JButton btnDeconnexion = new JButton("Se d\u00E9connecter");
+		btnDeconnexion.setForeground(Color.WHITE);
+		btnDeconnexion.setFont(new Font("Roboto", Font.BOLD, 13));
+		btnDeconnexion.setBackground(Couleur.ROUGE);
+		panelDeconnexion.add(btnDeconnexion);
+		
+		JPanel panelContenu = new JPanel();
+		panelContenu.setBackground(Couleur.BLEU1);
+		fenetreProfilJoueur.getContentPane().add(panelContenu, BorderLayout.CENTER);
+		GridBagLayout gbl_panelContenu = new GridBagLayout();
+		gbl_panelContenu.columnWidths = new int[]{1384, 0};
+		gbl_panelContenu.rowHeights = new int[] {250, 576, 0};
+		gbl_panelContenu.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelContenu.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelContenu.setLayout(gbl_panelContenu);
+		
+		JPanel panelHeader = new JPanel();
+		panelHeader.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelHeader = new GridBagConstraints();
+		gbc_panelHeader.fill = GridBagConstraints.BOTH;
+		gbc_panelHeader.insets = new Insets(0, 0, 5, 0);
+		gbc_panelHeader.gridx = 0;
+		gbc_panelHeader.gridy = 0;
+		panelContenu.add(panelHeader, gbc_panelHeader);
+		GridBagLayout gbl_panelHeader = new GridBagLayout();
+		gbl_panelHeader.columnWidths = new int[]{1384, 0};
+		gbl_panelHeader.rowHeights = new int[] {204, 40, 0};
+		gbl_panelHeader.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelHeader.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelHeader.setLayout(gbl_panelHeader);
+		
+		JPanel panelJoueur = new JPanel();
+		panelJoueur.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelJoueur = new GridBagConstraints();
+		gbc_panelJoueur.fill = GridBagConstraints.BOTH;
+		gbc_panelJoueur.insets = new Insets(0, 0, 5, 0);
+		gbc_panelJoueur.gridx = 0;
+		gbc_panelJoueur.gridy = 0;
+		panelHeader.add(panelJoueur, gbc_panelJoueur);
+		GridBagLayout gbl_panelJoueur = new GridBagLayout();
+		gbl_panelJoueur.columnWidths = new int[]{1384, 0};
+		gbl_panelJoueur.rowHeights = new int[] {138, 60, 0};
+		gbl_panelJoueur.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelJoueur.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelJoueur.setLayout(gbl_panelJoueur);
+		
+		JLabel photo = new JLabel("insererPhoto");
+		photo.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_photo = new GridBagConstraints();
+		gbc_photo.fill = GridBagConstraints.BOTH;
+		gbc_photo.insets = new Insets(0, 0, 5, 0);
+		gbc_photo.gridx = 0;
+		gbc_photo.gridy = 0;
+		panelJoueur.add(photo, gbc_photo);
+		
+		JLabel pseudo = new JLabel("PSEUDO");
+		pseudo.setFont(new Font("Roboto", Font.BOLD, 24));
+		pseudo.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_pseudo = new GridBagConstraints();
+		gbc_pseudo.fill = GridBagConstraints.BOTH;
+		gbc_pseudo.gridx = 0;
+		gbc_pseudo.gridy = 1;
+		panelJoueur.add(pseudo, gbc_pseudo);
+		
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout = (FlowLayout) panelMenu.getLayout();
+		flowLayout.setVgap(0);
+		GridBagConstraints gbc_panelMenu = new GridBagConstraints();
+		gbc_panelMenu.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelMenu.gridx = 0;
+		gbc_panelMenu.gridy = 1;
+		panelHeader.add(panelMenu, gbc_panelMenu);
+		
+		JButton btnProfil = new JButton("Mon profil");
+		btnProfil.setForeground(Color.WHITE);
+		btnProfil.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnProfil.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnProfil);
+		
+		JButton btnEquipes = new JButton("Equipes");
+		btnEquipes.setForeground(Color.WHITE);
+		btnEquipes.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnEquipes.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnEquipes);
+		
+		JButton btnTournois = new JButton("Tournois");
+		btnTournois.setForeground(Color.WHITE);
+		btnTournois.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnTournois.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnTournois);
+		
+		JButton btnClassement = new JButton("Classement");
+		btnClassement.setForeground(Color.WHITE);
+		btnClassement.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnClassement.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnClassement);
+		
+		JPanel panelProfil = new JPanel();
+		panelProfil.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelProfil = new GridBagConstraints();
+		gbc_panelProfil.fill = GridBagConstraints.BOTH;
+		gbc_panelProfil.gridx = 0;
+		gbc_panelProfil.gridy = 1;
+		panelContenu.add(panelProfil, gbc_panelProfil);
+		GridBagLayout gbl_panelProfil = new GridBagLayout();
+		gbl_panelProfil.columnWidths = new int[] {534, 850, 0};
+		gbl_panelProfil.rowHeights = new int[]{576, 0};
+		gbl_panelProfil.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelProfil.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelProfil.setLayout(gbl_panelProfil);
+		
+		JPanel panelEquipe = new JPanel();
+		panelEquipe.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelEquipe = new GridBagConstraints();
+		gbc_panelEquipe.fill = GridBagConstraints.BOTH;
+		gbc_panelEquipe.insets = new Insets(0, 0, 0, 5);
+		gbc_panelEquipe.gridx = 0;
+		gbc_panelEquipe.gridy = 0;
+		panelProfil.add(panelEquipe, gbc_panelEquipe);
+		GridBagLayout gbl_panelEquipe = new GridBagLayout();
+		gbl_panelEquipe.columnWidths = new int[] {534, 0};
+		gbl_panelEquipe.rowHeights = new int[] {75, 501, 0};
+		gbl_panelEquipe.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelEquipe.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelEquipe.setLayout(gbl_panelEquipe);
+		
+		JPanel panelTitreEquipe = new JPanel();
+		panelTitreEquipe.setBackground(Couleur.BLEU1);
+		FlowLayout fl_panelTitreEquipe = (FlowLayout) panelTitreEquipe.getLayout();
+		fl_panelTitreEquipe.setVgap(0);
+		fl_panelTitreEquipe.setAlignment(FlowLayout.LEFT);
+		fl_panelTitreEquipe.setHgap(50);
+		GridBagConstraints gbc_panelTitreEquipe = new GridBagConstraints();
+		gbc_panelTitreEquipe.anchor = GridBagConstraints.SOUTH;
+		gbc_panelTitreEquipe.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelTitreEquipe.insets = new Insets(0, 0, 5, 0);
+		gbc_panelTitreEquipe.gridx = 0;
+		gbc_panelTitreEquipe.gridy = 0;
+		panelEquipe.add(panelTitreEquipe, gbc_panelTitreEquipe);
+		
+		JLabel titreEquipe = new JLabel("Mon \u00E9quipe (insererNom)");
+		titreEquipe.setFont(new Font("Roboto", Font.BOLD, 20));
+		panelTitreEquipe.add(titreEquipe);
+		
+		JPanel panelJoueursEquipe = new JPanel();
+		panelJoueursEquipe.setBackground(Couleur.BLEU1);
+		FlowLayout fl_panelJoueursEquipe = (FlowLayout) panelJoueursEquipe.getLayout();
+		fl_panelJoueursEquipe.setVgap(20);
+		fl_panelJoueursEquipe.setHgap(50);
+		fl_panelJoueursEquipe.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panelJoueursEquipe = new GridBagConstraints();
+		gbc_panelJoueursEquipe.fill = GridBagConstraints.BOTH;
+		gbc_panelJoueursEquipe.gridx = 0;
+		gbc_panelJoueursEquipe.gridy = 1;
+		panelEquipe.add(panelJoueursEquipe, gbc_panelJoueursEquipe);
+		
+		JList listeJoueursEquipe = new JList();
+		listeJoueursEquipe.setFont(new Font("Roboto", Font.PLAIN, 15));
+		panelJoueursEquipe.add(listeJoueursEquipe);
+		
+		JPanel panelStats = new JPanel();
+		panelStats.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelStats = new GridBagConstraints();
+		gbc_panelStats.fill = GridBagConstraints.BOTH;
+		gbc_panelStats.gridx = 1;
+		gbc_panelStats.gridy = 0;
+		panelProfil.add(panelStats, gbc_panelStats);
+		GridBagLayout gbl_panelStats = new GridBagLayout();
+		gbl_panelStats.columnWidths = new int[] {850, 0};
+		gbl_panelStats.rowHeights = new int[] {75, 501, 0};
+		gbl_panelStats.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelStats.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelStats.setLayout(gbl_panelStats);
+		
+		JPanel panelTitreStats = new JPanel();
+		panelTitreStats.setBackground(Couleur.BLEU1);
+		FlowLayout fl_panelTitreStats = (FlowLayout) panelTitreStats.getLayout();
+		fl_panelTitreStats.setVgap(0);
+		fl_panelTitreStats.setHgap(50);
+		fl_panelTitreStats.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panelTitreStats = new GridBagConstraints();
+		gbc_panelTitreStats.anchor = GridBagConstraints.SOUTH;
+		gbc_panelTitreStats.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelTitreStats.insets = new Insets(0, 0, 5, 0);
+		gbc_panelTitreStats.gridx = 0;
+		gbc_panelTitreStats.gridy = 0;
+		panelStats.add(panelTitreStats, gbc_panelTitreStats);
+		
+		JLabel titreStats = new JLabel("Statistiques \u00E9quipe");
+		titreStats.setFont(new Font("Roboto", Font.BOLD, 20));
+		panelTitreStats.add(titreStats);
+		
+		JPanel panelListeStats = new JPanel();
+		panelListeStats.setBackground(Couleur.BLEU1);
+		FlowLayout fl_panelListeStats = (FlowLayout) panelListeStats.getLayout();
+		fl_panelListeStats.setVgap(20);
+		fl_panelListeStats.setAlignment(FlowLayout.LEFT);
+		fl_panelListeStats.setHgap(50);
+		GridBagConstraints gbc_panelListeStats = new GridBagConstraints();
+		gbc_panelListeStats.fill = GridBagConstraints.BOTH;
+		gbc_panelListeStats.gridx = 0;
+		gbc_panelListeStats.gridy = 1;
+		panelStats.add(panelListeStats, gbc_panelListeStats);
+		
+		JList listeStats = new JList();
+		listeStats.setFont(new Font("Roboto", Font.PLAIN, 15));
+		panelListeStats.add(listeStats);
+	}
+
+}
---
+++ SAE3/src/vue/VueTournoisJoueur.java
@@ -0,0 +1,296 @@
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.LineBorder;
+
+public class VueTournoisJoueur {
+
+	private JFrame fenetreTournoisJoueur;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VueTournoisJoueur window = new VueTournoisJoueur();
+					window.fenetreTournoisJoueur.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private VueTournoisJoueur() {
+		fenetreTournoisJoueur = new JFrame();
+		fenetreTournoisJoueur.getContentPane().setBackground(Couleur.BLEU1);
+		fenetreTournoisJoueur.setResizable(false);
+		fenetreTournoisJoueur.setBounds(100, 100, 1400, 900);
+		fenetreTournoisJoueur.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelDeconnexion = new JPanel();
+		panelDeconnexion.setBackground(Color.WHITE);
+		fenetreTournoisJoueur.getContentPane().add(panelDeconnexion, BorderLayout.NORTH);
+		panelDeconnexion.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JButton btnDeconnexion = new JButton("Se d\u00E9connecter");
+		btnDeconnexion.setForeground(Color.WHITE);
+		btnDeconnexion.setFont(new Font("Roboto", Font.BOLD, 13));
+		btnDeconnexion.setBackground(Couleur.ROUGE);
+		panelDeconnexion.add(btnDeconnexion);
+		
+		JPanel panelContenu = new JPanel();
+		panelContenu.setBackground(Couleur.BLEU1);
+		fenetreTournoisJoueur.getContentPane().add(panelContenu, BorderLayout.CENTER);
+		GridBagLayout gbl_panelContenu = new GridBagLayout();
+		gbl_panelContenu.columnWidths = new int[]{1384, 0};
+		gbl_panelContenu.rowHeights = new int[] {250, 576, 0};
+		gbl_panelContenu.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelContenu.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelContenu.setLayout(gbl_panelContenu);
+		
+		JPanel panelHeader = new JPanel();
+		panelHeader.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelHeader = new GridBagConstraints();
+		gbc_panelHeader.fill = GridBagConstraints.BOTH;
+		gbc_panelHeader.insets = new Insets(0, 0, 5, 0);
+		gbc_panelHeader.gridx = 0;
+		gbc_panelHeader.gridy = 0;
+		panelContenu.add(panelHeader, gbc_panelHeader);
+		GridBagLayout gbl_panelHeader = new GridBagLayout();
+		gbl_panelHeader.columnWidths = new int[]{1384, 0};
+		gbl_panelHeader.rowHeights = new int[] {204, 40, 0};
+		gbl_panelHeader.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelHeader.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelHeader.setLayout(gbl_panelHeader);
+		
+		JPanel panelJoueur = new JPanel();
+		panelJoueur.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelJoueur = new GridBagConstraints();
+		gbc_panelJoueur.fill = GridBagConstraints.BOTH;
+		gbc_panelJoueur.insets = new Insets(0, 0, 5, 0);
+		gbc_panelJoueur.gridx = 0;
+		gbc_panelJoueur.gridy = 0;
+		panelHeader.add(panelJoueur, gbc_panelJoueur);
+		GridBagLayout gbl_panelJoueur = new GridBagLayout();
+		gbl_panelJoueur.columnWidths = new int[]{1384, 0};
+		gbl_panelJoueur.rowHeights = new int[] {138, 60, 0};
+		gbl_panelJoueur.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelJoueur.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panelJoueur.setLayout(gbl_panelJoueur);
+		
+		JLabel photo = new JLabel("insererPhoto");
+		photo.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_photo = new GridBagConstraints();
+		gbc_photo.fill = GridBagConstraints.BOTH;
+		gbc_photo.insets = new Insets(0, 0, 5, 0);
+		gbc_photo.gridx = 0;
+		gbc_photo.gridy = 0;
+		panelJoueur.add(photo, gbc_photo);
+		
+		JLabel pseudo = new JLabel("PSEUDO");
+		pseudo.setFont(new Font("Roboto", Font.BOLD, 24));
+		pseudo.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_pseudo = new GridBagConstraints();
+		gbc_pseudo.fill = GridBagConstraints.BOTH;
+		gbc_pseudo.gridx = 0;
+		gbc_pseudo.gridy = 1;
+		panelJoueur.add(pseudo, gbc_pseudo);
+		
+		JPanel panelMenu = new JPanel();
+		panelMenu.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout = (FlowLayout) panelMenu.getLayout();
+		flowLayout.setVgap(0);
+		GridBagConstraints gbc_panelMenu = new GridBagConstraints();
+		gbc_panelMenu.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelMenu.gridx = 0;
+		gbc_panelMenu.gridy = 1;
+		panelHeader.add(panelMenu, gbc_panelMenu);
+		
+		JButton btnProfil = new JButton("Mon profil");
+		btnProfil.setForeground(Color.WHITE);
+		btnProfil.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnProfil.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnProfil);
+		
+		JButton btnEquipes = new JButton("Equipes");
+		btnEquipes.setForeground(Color.WHITE);
+		btnEquipes.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnEquipes.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnEquipes);
+		
+		JButton btnTournois = new JButton("Tournois");
+		btnTournois.setForeground(Color.WHITE);
+		btnTournois.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnTournois.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnTournois);
+		
+		JButton btnClassement = new JButton("Classement");
+		btnClassement.setForeground(Color.WHITE);
+		btnClassement.setFont(new Font("Roboto", Font.BOLD, 15));
+		btnClassement.setBackground(Couleur.BLEU2);
+		panelMenu.add(btnClassement);
+		
+		JPanel panelProfil = new JPanel();
+		panelProfil.setBackground(Couleur.BLEU1);
+		GridBagConstraints gbc_panelProfil = new GridBagConstraints();
+		gbc_panelProfil.fill = GridBagConstraints.BOTH;
+		gbc_panelProfil.gridx = 0;
+		gbc_panelProfil.gridy = 1;
+		panelContenu.add(panelProfil, gbc_panelProfil);
+		panelProfil.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Couleur.BLEU1);
+		panelProfil.add(panel);
+		panel.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(Color.WHITE));
+		panel_3.setBackground(Couleur.BLEU1);
+		panel.add(panel_3);
+		GridBagLayout gbl_panel_3 = new GridBagLayout();
+		gbl_panel_3.columnWidths = new int[]{692, 0};
+		gbl_panel_3.rowHeights = new int[] {100, 188, 0};
+		gbl_panel_3.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_3.setLayout(gbl_panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_1 = (FlowLayout) panel_4.getLayout();
+		flowLayout_1.setVgap(0);
+		flowLayout_1.setHgap(50);
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
+		gbc_panel_4.anchor = GridBagConstraints.SOUTH;
+		gbc_panel_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_4.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_4.gridx = 0;
+		gbc_panel_4.gridy = 0;
+		panel_3.add(panel_4, gbc_panel_4);
+		
+		JLabel tournoisEnCours = new JLabel("Tournois en cours");
+		tournoisEnCours.setFont(new Font("Roboto", Font.BOLD, 20));
+		panel_4.add(tournoisEnCours);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_2 = (FlowLayout) panel_5.getLayout();
+		flowLayout_2.setHgap(50);
+		flowLayout_2.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panel_5 = new GridBagConstraints();
+		gbc_panel_5.fill = GridBagConstraints.BOTH;
+		gbc_panel_5.gridx = 0;
+		gbc_panel_5.gridy = 1;
+		panel_3.add(panel_5, gbc_panel_5);
+		
+		JList list = new JList();
+		panel_5.add(list);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(Color.WHITE));
+		panel_2.setBackground(Couleur.BLEU1);
+		panel.add(panel_2);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{692, 0};
+		gbl_panel_2.rowHeights = new int[] {100, 188, 0};
+		gbl_panel_2.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_3 = (FlowLayout) panel_6.getLayout();
+		flowLayout_3.setVgap(0);
+		flowLayout_3.setHgap(50);
+		flowLayout_3.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panel_6 = new GridBagConstraints();
+		gbc_panel_6.anchor = GridBagConstraints.SOUTH;
+		gbc_panel_6.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_6.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_6.gridx = 0;
+		gbc_panel_6.gridy = 0;
+		panel_2.add(panel_6, gbc_panel_6);
+		
+		JLabel mesTournois = new JLabel("Mes tournois");
+		mesTournois.setFont(new Font("Roboto", Font.BOLD, 20));
+		panel_6.add(mesTournois);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_4 = (FlowLayout) panel_7.getLayout();
+		flowLayout_4.setAlignment(FlowLayout.LEFT);
+		flowLayout_4.setHgap(50);
+		GridBagConstraints gbc_panel_7 = new GridBagConstraints();
+		gbc_panel_7.fill = GridBagConstraints.BOTH;
+		gbc_panel_7.gridx = 0;
+		gbc_panel_7.gridy = 1;
+		panel_2.add(panel_7, gbc_panel_7);
+		
+		JList list_1 = new JList();
+		panel_7.add(list_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(Color.WHITE));
+		panel_1.setBackground(Couleur.BLEU1);
+		panelProfil.add(panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{692, 0};
+		gbl_panel_1.rowHeights = new int[] {100, 476, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_5 = (FlowLayout) panel_9.getLayout();
+		flowLayout_5.setVgap(0);
+		flowLayout_5.setHgap(50);
+		flowLayout_5.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panel_9 = new GridBagConstraints();
+		gbc_panel_9.anchor = GridBagConstraints.SOUTH;
+		gbc_panel_9.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_9.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_9.gridx = 0;
+		gbc_panel_9.gridy = 0;
+		panel_1.add(panel_9, gbc_panel_9);
+		
+		JLabel tournoisAVenir = new JLabel("Tournois \u00E0 venir");
+		tournoisAVenir.setFont(new Font("Roboto", Font.BOLD, 20));
+		panel_9.add(tournoisAVenir);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(Couleur.BLEU1);
+		FlowLayout flowLayout_6 = (FlowLayout) panel_8.getLayout();
+		flowLayout_6.setHgap(50);
+		flowLayout_6.setAlignment(FlowLayout.LEFT);
+		GridBagConstraints gbc_panel_8 = new GridBagConstraints();
+		gbc_panel_8.fill = GridBagConstraints.BOTH;
+		gbc_panel_8.gridx = 0;
+		gbc_panel_8.gridy = 1;
+		panel_1.add(panel_8, gbc_panel_8);
+		
+		JList list_2 = new JList();
+		panel_8.add(list_2);
+	}
+
+}
